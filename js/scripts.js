025b475a7a962916a54893d2ace276c901ad9aaf
Yehuda Nathanael <52704652+yudachris@users.noreply.github.com>
2020-02-18 23:54:28 +0900

    Update 20200218
    - Tweaking navbar and adding some script for hide/show
    - Change about me s.ection Text and layout.
    - Change education and experience section layout.
    
    * NEXT : BIG THING FOR THE NAVIGATION

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var menus = document.querySelectorAll(".navigation__menu");
+var menus = $(".navigation__item");
 var numberOfMenu = menus.length;
 
 for(var i = 0; i < numberOfMenu; i++){
@@ -8,6 +8,10 @@ for(var i = 0; i < numberOfMenu; i++){
         var buttonId = this.id;
 
         switch(buttonId) {
+            case "abt":
+                //#section-about
+                document.querySelector("#section-about").scrollIntoView({behavior:"smooth"});
+            break;
             case "edu":
                 //#section-education
                 document.querySelector("#section-education").scrollIntoView({behavior:"smooth"});
@@ -27,3 +31,15 @@ for(var i = 0; i < numberOfMenu; i++){
     }); 
 
 }
+
+
+//SCROLLING FUNCTION
+
+$(".wrapper").on('scroll', function(){
+    
+    var wrapperScroll = $(".wrapper").scrollTop();
+    var headerHeight = $(".header").height();
+    
+    $(".navigation").toggleClass("hide", wrapperScroll >= headerHeight);
+    
+});
