2f2db97f786e0f1cff9d63406225e23a49a8126a
Yehuda Nathanael <52704652+yudachris@users.noreply.github.com>
2020-02-19 23:20:25 +0900

    Update 20200219
    -Added javascript for mobile navigation
    -Added vendor specific prefixes
    -Debug for animation in edge
    -Unresolved: parallax in edge.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,62 @@
 var menus = $(".navigation__item");
+var menusMobile = $(".navigation-mobile__item");
 var numberOfMenu = menus.length;
 
-for(var i = 0; i < numberOfMenu; i++){
+if(menus.length != menusMobile.length){
+    alert("The number of menus in browser and mobile version do not match. The menu will not work. Please do correction.");
+}
+else {
+    for(var i = 0; i < numberOfMenu; i++){
     
-    menus[i].addEventListener("click", function(e){
+        menus[i].addEventListener("click", function(e){
+            
+            var buttonId = this.id;
+    
+            scrollToSection(buttonId);
+    
+        });
         
-        var buttonId = this.id;
-
-        switch(buttonId) {
-            case "abt":
-                //#section-about
-                document.querySelector("#section-about").scrollIntoView({behavior:"smooth"});
-            break;
-            case "edu":
-                //#section-education
-                document.querySelector("#section-education").scrollIntoView({behavior:"smooth"});
-            break;
-            case "exp":
-                //#section-work-experience
-                document.querySelector("#section-work-experience").scrollIntoView({behavior:"smooth"});
-            break;
-            case "con":
-                //section-contact
-                document.querySelector("#section-contact").scrollIntoView({behavior:"smooth"});
-            break;
-            default:
-                alert("Something impossible just happenned.");
-        }
-
-    }); 
-
+        menusMobile[i].addEventListener("click", function(e){
+            
+            var buttonId = this.id;
+    
+            scrollToSection(buttonId);
+    
+        });
+    
+    }
 }
 
+function scrollToSection (buttonId) {
+    switch(buttonId) {
+        case "abt":
+            //#section-about
+            document.querySelector("#section-about").scrollIntoView({behavior:"smooth"});
+        break;
+        case "edu":
+            //#section-education
+            document.querySelector("#section-education").scrollIntoView({behavior:"smooth"});
+        break;
+        case "exp":
+            //#section-work-experience
+            document.querySelector("#section-work-experience").scrollIntoView({behavior:"smooth"});
+        break;
+        case "con":
+            //section-contact
+            document.querySelector("#section-contact").scrollIntoView({behavior:"smooth"});
+        break;
+        default:
+            alert("Something impossible just happenned.");
+    }
+}
 
 //SCROLLING FUNCTION
-
 $(".wrapper").on('scroll', function(){
-    
+
     var wrapperScroll = $(".wrapper").scrollTop();
     var headerHeight = $(".header").height();
     
     $(".navigation").toggleClass("hide", wrapperScroll >= headerHeight);
     
 });
+
